a16bf234cf3c183a6b3d60a0951377832fe29587
nyaaak25 <kazama.akira.s2@dc.tohoku.ac.jp>
2022-03-21 14:55:48 +0900

    ADD Instrument function !

--- intensity.py
+++ intensity.py
@@ -8,14 +8,17 @@ ver. 1.0: 計算された光学的厚みから放射強度に変換
 Created on Thu Feb 17 22:53:00 2022
 
 ver. 2.0: OMEGA Insturment function導入
-created on xxxx
+created on Mon Mar 21 14:48:00 2022
 """
 
-
+# library import
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal as signal
+import scipy.stats as stats
 
-Tau_txt = np.loadtxt('4545-5556_0.001step_old.txt')
+# Optical Depth >> Intensity
+Tau_txt = np.loadtxt('4545-5556_0.01step_cutoff_120.txt')
 Tau = Tau_txt[:, 1]
 v = Tau_txt[:, 0]
 sza_theta = 18.986036
@@ -26,10 +29,25 @@ Iobs = I0 * np.exp(-Tau)
 x1 = v
 y1 = Iobs
 
+
+# 装置関数実装部分
+# 畳み込む装置関数定義。OMEGAはGAUSSIUN
+mu_conv = 0.0  # Centroid
+sigma_conv = 0.5  # Width
+convolving_term = stats.norm(mu_conv, sigma_conv)
+xconv = np.linspace(-20, 20, 4000)
+yconv = convolving_term.pdf(xconv)
+
+# 畳み込みを行う部分
+convolved_pdf = signal.convolve(Iobs, yconv, mode='same')/sum(Iobs)
+
+
+# plot部分
 fig = plt.figure()
 ax = fig.add_subplot(111, title='CO2')
 ax.grid(c='lightgray', zorder=1)
-ax.plot(x1, y1, color='b', linewidth=0.1)
+# ax.plot(x1, y1, color='b', linewidth=0.1)
+ax.plot(v, convolved_pdf, color='b')
 # ax.set_xlim(4973, 4975)
 # ax.set_yscale('log')
 ax.set_xlabel('Wavenumber [$cm^{-1}$]', fontsize=14)
