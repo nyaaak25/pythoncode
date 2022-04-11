22b70b5deac1d4f1881c7a0708df4c3a3b1527d2
nyaaak25 <kazama.akira.s2@dc.tohoku.ac.jp>
2022-04-11 17:56:45 +0900

    pre-temp profile make

--- nyooon.py
+++ nyooon.py
@@ -1,5 +1,6 @@
 # %%
 # インポート
+import matplotlib.pylab as plt
 from pyrsistent import v
 from scipy.io import readsav
 import scipy.io as sio
@@ -12,8 +13,45 @@ import time
 from numba import jit, f8
 
 # %%
+# 気圧と温度プロファイルを導出する
+
 import numpy as np
-import matplotlib.pylab as plt
+
+# parameter指定 MKS単位系
+g = 3.72  # m s-1
+# R = 8.314  # E+7  # (g*cm^2*s^2)/(mol*K)
+R = 192
+T1 = [160, 213, 260]  # K
+T2 = [80, 146, 200]  # K
+Surface_pressure = [50, 150, 180, 215, 257, 308,
+                    369, 442, 529, 633, 758, 907, 1096, 1300, 1500]  # Pa
+
+# hight profile
+Hight_km = np.arange(0, 62, 2)  # km
+Hight = Hight_km * 1000  # m
+
+# Scale Hight
+SH = (R*T1[1])/g
+
+# pressure profile
+pre = Surface_pressure[9] * np.exp(-Hight/SH)
+
+# Temp profile
+H1 = SH*0.1
+H2 = SH*4.0
+
+a = (T1[1]-T2[1])/(H1-H2)
+b = T1[1]-a*H1
+
+Temp = np.zeros(len(Hight))
+for i in range(len(Hight)):
+    if Hight[i] <= H2:
+        Temp[i] = a * Hight[i] + b
+    else:
+        Temp[i] = T2[1]
+
+print(Temp)
+# %%
 
 # 波数
 v_txt = np.loadtxt('4545-5556_0.01step_cutoff_120.txt')
