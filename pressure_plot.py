60b45deee775bbadebe42e3a767620cfe95c00a0
nyaaak25 <kazama.akira.s2@dc.tohoku.ac.jp>
2022-07-02 18:05:54 +0900

    lut内のいろいろをplotしてみた

--- pressure_plot.py
+++ pressure_plot.py
@@ -1,104 +1,58 @@
+"""
+-*- coding: utf-8 -*-
+LUT内の気圧精度確認するプログラム
+@author: A.Kazama kazama@pparc.gp.tohoku.ac.jp
+
+ver. 1.0: 気圧変化をplotし、どのくらいの精度があるかを確認
+Created on Tue Jun 28 17:48:00 2022
+
+"""
 # %%
 # インポート
 import matplotlib.pylab as plt
 import numpy as np
 
-# SZA＝0、EA=0, PA=0, TA=135, TB=80, SurfaceA=0.05, waterI=0, Dust=0
-# pressure index 1 :: 50 Pa
-ARS_1 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP1_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x1 = ARS_1[:, 0]
-ARS_x1 = (1/ARS_x1)*10000
-ARS_x1 = ARS_x1[::-1]
-ARS_y1 = ARS_1[:, 1]
-ARS_y1 = ARS_y1[::-1]
-
-# pressure index 2 :: 150 Pa
-ARS_2 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP2_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x2 = ARS_2[:, 0]
-ARS_x2 = (1/ARS_x2)*10000
-ARS_x2 = ARS_x2[::-1]
-ARS_y2 = ARS_2[:, 1]
-ARS_y2 = ARS_y2[::-1]
-
-# pressure index 3 :: 180 Pa
-ARS_3 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP3_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x3 = ARS_3[:, 0]
-ARS_x3 = (1/ARS_x3)*10000
-ARS_x3 = ARS_x3[::-1]
-ARS_y3 = ARS_3[:, 1]
-ARS_y3 = ARS_y3[::-1]
-
-# pressure index 4 :: 215 Pa
-ARS_4 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP4_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x4 = ARS_4[:, 0]
-ARS_x4 = (1/ARS_x4)*10000
-ARS_x4 = ARS_x4[::-1]
-ARS_y4 = ARS_4[:, 1]
-ARS_y4 = ARS_y4[::-1]
-
-# pressure index 5 :: 257 Pa
-ARS_5 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP5_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x5 = ARS_5[:, 0]
-ARS_x5 = (1/ARS_x5)*10000
-ARS_x5 = ARS_x5[::-1]
-ARS_y5 = ARS_5[:, 1]
-ARS_y5 = ARS_y5[::-1]
-
-# pressure index 56:: 308 Pa
-ARS_6 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP6_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x6 = ARS_6[:, 0]
-ARS_x6 = (1/ARS_x6)*10000
-ARS_x6 = ARS_x6[::-1]
-ARS_y6 = ARS_6[:, 1]
-ARS_y6 = ARS_y6[::-1]
-
-# pressure index 7 :: 369 Pa
-ARS_7 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP7_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x7 = ARS_7[:, 0]
-ARS_x7 = (1/ARS_x7)*10000
-ARS_x7 = ARS_x7[::-1]
-ARS_y7 = ARS_7[:, 1]
-ARS_y7 = ARS_y7[::-1]
-
-# pressure index 8 :: 442 Pa
-ARS_8 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP8_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x8 = ARS_8[:, 0]
-ARS_x8 = (1/ARS_x8)*10000
-ARS_x8 = ARS_x8[::-1]
-ARS_y8 = ARS_8[:, 1]
-ARS_y8 = ARS_y8[::-1]
-
-# pressure index 9 :: 529 Pa
-ARS_9 = np.loadtxt(
-    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP9_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
-ARS_x9 = ARS_9[:, 0]
-ARS_x9 = (1/ARS_x9)*10000
-ARS_x9 = ARS_x9[::-1]
-ARS_y9 = ARS_9[:, 1]
-ARS_y9 = ARS_y9[::-1]
-
 fig = plt.figure(dpi=200)
 ax = fig.add_subplot(111, title='CO2 absorption')
 ax.grid(c='lightgray', zorder=1)
-ax.plot(ARS_x1, ARS_y1, color='blue', label="Pa = 50", zorder=1)
-ax.plot(ARS_x2, ARS_y2, color='red', label="Pa = 150", zorder=2)
-ax.plot(ARS_x3, ARS_y3, color='green', label="Pa = 180", zorder=3)
-ax.plot(ARS_x4, ARS_y4, color='orange', label="Pa = 215", zorder=4)
-ax.plot(ARS_x5, ARS_y5, color='pink', label="Pa = 257", zorder=5)
-ax.plot(ARS_x6, ARS_y6, color='purple', label="Pa = 308", zorder=6)
-ax.plot(ARS_x7, ARS_y7, color='skyblue', label="Pa = 369", zorder=7)
-ax.plot(ARS_x8, ARS_y8, color='yellow', label="Pa = 442", zorder=8)
-ax.plot(ARS_x9, ARS_y9, color='black', label="Pa = 529", zorder=8)
+ax.set_xlabel('Wavenumber [μm]', fontsize=10)
+ax.set_ylabel('Radiande', fontsize=10)
+
+Pa_list = ['Pa=50', 'Pa=150', 'Pa=180', 'Pa=215', 'Pa=257', 'Pa=308', 'Pa=369',
+           'Pa=442', 'Pa=529', 'Pa=633', 'Pa=758', 'Pa=907', 'Pa=1096', 'Pa=1300', 'Pa=1500']
+TA_list = ['TA=135', 'TA=160', 'TA=213', 'TA=260', 'TA=285']
+TB_list = ['TB=80', 'TB=146', 'TB=200']
+SZA_list = ['SZA=0', 'SZA=15', 'SZA=30', 'SZA=45', 'SZA=60', 'SZA=75']
+EA_list = ['EA=0', 'EA=5', 'EA=10']
+PA_list = ['PA=0', 'PA=45', 'PA=90', 'PA=135', 'PA=180']
+Dust_list = ['Dust=0', 'Dust=0.3', 'Dust=0.6',
+             'Dust=0.9', 'Dust=1.2', 'Dust=1.5']
+WaterI_list = ['WaterI=0', 'WaterI=0.5', 'WaterI=1.0']
+Albedo_list = ['Albedo=0.05', 'Albedo=0.1', 'ALbedo=0.2',
+               'Albedo=0.3', 'Albedo=0.4', 'Albedo=0.5']
+
+# SZA＝0、EA=0, PA=0, TA=135, TB=80, SurfaceA=0.05, waterI=0, Dust=0
+for i in range(1, 16, 1):
+    ARS = np.loadtxt('/Users/nyonn/Desktop/pythoncode/ARS_calc/SP' +
+                     str(i)+'_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
+    ARS_x = ARS[:, 0]
+    ARS_x = (1/ARS_x)*10000
+    ARS_x = ARS_x[::-1]
+    ARS_y = ARS[:, 1]
+    ARS_y = ARS_y[::-1]
+
+    POLY_x = [ARS_x[0], ARS_x[3], ARS_x[5], ARS_x[23], ARS_x[24], ARS_x[25]]
+    POLY_y = [ARS_y[0], ARS_y[3], ARS_y[5], ARS_y[23], ARS_y[24], ARS_y[25]]
+    a, b = np.polyfit(POLY_x, POLY_y, 1)
+
+    cont0 = b + a*ARS_x
+    y_calc = 1 - ARS_y/cont0
+    y_total = np.sum(y_calc[7:17])
+
+    ax.plot(ARS_x, ARS_y, label=Pa_list[i-1], zorder=i)
+    print(y_total)
 
 h1, l1 = ax.get_legend_handles_labels()
-ax.legend(h1, l1, loc='lower right', fontsize=8)
+ax.legend(h1, l1, loc='lower right', fontsize=5)
 plt.show()
 # %%
