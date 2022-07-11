9e0fc5682d511d576bf012ada2e426bc75ef604b
nyaaak25 <kazama.akira.s2@dc.tohoku.ac.jp>
2022-07-11 11:52:47 +0900

    retrieval_test

--- nyoon.py
+++ nyoon.py
@@ -1,3 +1,5 @@
+# %%
+# LUTと観測データの吸収量を確認できるプログラム
 import matplotlib.pylab as plt
 import numpy as np
 from scipy.io import readsav
@@ -5,11 +7,15 @@ import scipy.io as sio
 from os.path import dirname, join as pjoin
 
 data_dir = pjoin(dirname(sio.__file__), 'tests', 'data')
-sav_fname_obs = '/Users/nyonn/IDLWorkspace/Default/savfile/Table_SP_obs_calc_orb0920_3.sav'
+sav_fname_obs = '/Users/nyonn/IDLWorkspace/Default/savfile/Table_SP_obs_calc_orb0931_3.sav'
 sav_data_obs = readsav(sav_fname_obs)
 
 sav_fname_lut = '/Users/nyonn/IDLWorkspace/Default/savfile/Table_SP_Trans_calc.sav'
 sav_data_lut = readsav(sav_fname_lut)
+print(sav_data_lut.keys())
 
 pressure15 = sav_data_lut['table_equivalent_pressure15']
+pressure13 = sav_data_lut['table_equivalent_pressure13']
+pressure1 = sav_data_lut['table_equivalent_pressure1']
 observation = sav_data_obs['obs_spec']
+# %%
--- pressure_plot.py
+++ pressure_plot.py
@@ -16,7 +16,7 @@ fig = plt.figure(dpi=200)
 ax = fig.add_subplot(111, title='CO2 absorption')
 ax.grid(c='lightgray', zorder=1)
 ax.set_xlabel('Wavenumber [μm]', fontsize=10)
-ax.set_ylabel('Radiande', fontsize=10)
+ax.set_ylabel('Diference', fontsize=10)
 
 Pa_list = ['Pa=50', 'Pa=150', 'Pa=180', 'Pa=215', 'Pa=257', 'Pa=308', 'Pa=369',
            'Pa=442', 'Pa=529', 'Pa=633', 'Pa=758', 'Pa=907', 'Pa=1096', 'Pa=1300', 'Pa=1500']
@@ -31,6 +31,58 @@ WaterI_list = ['WaterI=0', 'WaterI=0.5', 'WaterI=1.0']
 Albedo_list = ['Albedo=0.05', 'Albedo=0.1', 'ALbedo=0.2',
                'Albedo=0.3', 'Albedo=0.4', 'Albedo=0.5']
 
+for i in range(1, 15, 1):
+    ARS = np.loadtxt('/Users/nyonn/Desktop/pythoncode/ARS_calc/SP' +
+                     str(i)+'_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
+    ARS_x = ARS[:, 0]
+    ARS_x = (1/ARS_x)*10000
+    ARS_x = ARS_x[::-1]
+    ARS_y = ARS[:, 1]
+    ARS_y = ARS_y[::-1]
+
+    ARS1 = np.loadtxt('/Users/nyonn/Desktop/pythoncode/ARS_calc/SP' +
+                      str(i+1)+'_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
+    ARS_x1 = ARS1[:, 0]
+    ARS_x1 = (1/ARS_x)*10000
+    ARS_x1 = ARS_x1[::-1]
+    ARS_y1 = ARS1[:, 1]
+    ARS_y1 = ARS_y1[::-1]
+
+    error1 = (ARS_y1 - ARS_y) * 100 / ARS_y
+
+    ax.plot(ARS_x, error1, label=Pa_list[i]+"-"+Pa_list[i-1])
+    ax.set_xlim(1.81, 1.825)
+    ax.set_ylim(-1e-15, 1e-15)
+h1, l1 = ax.get_legend_handles_labels()
+ax.legend(h1, l1, loc='lower right', fontsize=5)
+
+
+"""
+# どのくらいの差分があるかをみる
+ARS = np.loadtxt(
+    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP1_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
+ARS_x = ARS[:, 0]
+ARS_x = (1/ARS_x)*10000
+ARS_x = ARS_x[::-1]
+ARS_y = ARS[:, 1]
+ARS_y = ARS_y[::-1]
+
+ARS1 = np.loadtxt(
+    '/Users/nyonn/Desktop/pythoncode/ARS_calc/SP2_TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1_rad.dat')
+ARS_x1 = ARS1[:, 0]
+ARS_x1 = (1/ARS_x)*10000
+ARS_x1 = ARS_x1[::-1]
+ARS_y1 = ARS1[:, 1]
+ARS_y1 = ARS_y1[::-1]
+
+error1 = (ARS_y1 - ARS_y) * 100 / ARS_y
+
+
+ax.plot(ARS_x, error1)
+"""
+
+# %%
+# 気圧プロファイルを作成する
 # SZA＝0、EA=0, PA=0, TA=135, TB=80, SurfaceA=0.05, waterI=0, Dust=0
 for i in range(1, 16, 1):
     ARS = np.loadtxt('/Users/nyonn/Desktop/pythoncode/ARS_calc/SP' +
@@ -54,5 +106,7 @@ for i in range(1, 16, 1):
 
 h1, l1 = ax.get_legend_handles_labels()
 ax.legend(h1, l1, loc='lower right', fontsize=5)
+fig.savefig(
+    "/Users/nyonn/Desktop/plot_image/TA1_TB1_SZA1_EA1_PA1_Dust1_WaterI1_SurfaceA1.png")
 plt.show()
 # %%
